35c76ff6676a737b7f09a59d56f9d3b17caa8184
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2019-10-10 18:48:44 -0300

    Merge test initiated

--- test.cpp
+++ test.cpp
@@ -5,5 +5,6 @@ int main () {
 	//testing auto signing with GPG keys
 	//change from GitHub 10.10.19: 18:21:00
 	//change from GitHub 10.10.19: 18:36:00
+	//change from GitHub 10.10.19: 18:48:00 Hu Hu
 	return 0;
 }
