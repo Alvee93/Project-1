12b2625d8625d5eb8ee2a23726abf99a0ce0e41f
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2019-10-10 18:37:20 -0300

    Just a change to learn the Git Merge

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main () {
 	cout << "Hello From VM!" << endl;
 	//testing auto signing with GPG keys
 	//change from GitHub 10.10.19: 18:21:00
+	//change from GitHub 10.10.19: 18:36:00
 	return 0;
 }
