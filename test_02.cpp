e2d01c812e877fef66a87bde9a2b6276f81116cc
alvee-t <alvee.bitmascot@gmail.com>
2019-10-14 02:21:34 -0300

    First commit from personal VM
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test_02.cpp
+++ test_02.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main () {
-	//program desc added from PC!
+	//program desc added from PC
 	int a, b, c; //added int a, b, c on master
-	cout << "Hello World";
+	cout << "Hello World" << endl; //added endl from VM
 	return 0;
 }
