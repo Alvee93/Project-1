d6101cb66b29d0804c5fadc7a167fc0d92bc748c
alvee-t <alvee.bitmascot@gmail.com>
2019-10-14 12:20:50 -0300

    Corrected blank lines of the file
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test_02.cpp
+++ test_02.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main () {
-
 	//program desc added from PC
 	int a, b, c; //added int a, b, c on master
 	cout << "Hello World" << endl; //added endl from VM
+	
 	return 0;
 }
