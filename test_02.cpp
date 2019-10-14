48e99fcea52f4ecfc4417243803dc9b31623028b
alvee-t <alvee.bitmascot@gmail.com>
2019-10-14 12:16:07 -0300

    Cleaned test_02.cpp file
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test_02.cpp
+++ test_02.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main () {
+
 	//program desc added from PC
 	int a, b, c; //added int a, b, c on master
 	cout << "Hello World" << endl; //added endl from VM
