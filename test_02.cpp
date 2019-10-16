7b4d5a187efbf91a6619d6fb764b89655991bb1c
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2019-10-16 02:16:46 -0300

    Merge Con Initialisation

--- test_02.cpp
+++ test_02.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main () {
-	//program desc added from PC
+	//program desc added from PC!
 	int a, b, c; //added int a, b, c on master
 	cout << "Hello World";
 	return 0;
