c7aa188d5b5107e6529abe7697e825e6e3946445
alvee-t <alvee.bitmascot@gmail.com>
2019-10-15 22:15:36 -0300

    New line from VM with space
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test_02.cpp
+++ test_02.cpp
@@ -4,6 +4,6 @@ int main () {
 	//program desc added from PC
 	int a, b, c; //added int a, b, c on master
 	cout << "Hello World" << endl; //added endl from VM
-	cout << "New line added from VM"<< endl;
+	cout << "New line added from VM" << endl;
 	return 0;
 }
