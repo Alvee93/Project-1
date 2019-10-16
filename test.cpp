2f2873a9c0731a4fd41f483f0d22f8091e76eb30
alvee-t <alvee.bitmascot@gmail.com>
2019-10-15 23:35:31 -0300

    after rebase
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main () {
 	cout << "Hello From VM!" << endl;
+
 	//testing auto signing with GPG keys
 	//change from GitHub 10.10.19: 18:21:00
 	//change from GitHub 10.10.19: 18:36:00
