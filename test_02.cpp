c4469b35a29080f96a92419f357a55a51ae1197f
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2019-10-17 13:48:19 -0300

    Final merge line #9

--- test_02.cpp
+++ test_02.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main () {
 	//program desc added from PC!
-	int a, b, c; //added int a, b, c on master
+	int a, b, c, d; //added int a, b, c on master
 	cout << "Hello World";
 	
 	//this is updated from GH: 17-10-19: 12:33:00
+	//final merge from GH
 	return 0;
 }
