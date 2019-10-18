9ad3a5004d7ae9ce5afe8740ac877d09a9740918
alvee-t <alvee.bitmascot@gmail.com>
2019-10-17 22:55:43 -0300

    This commit is introduced for the genjam msg from VM
    - Updated msg
    - Waiting for the genjam
    
    Signed-off-by: alvee-t <alvee.bitmascot@gmail.com>

--- test_03.cpp
+++ test_03.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 int main (void) {
 	cout <<"Hello from test no. 03";
+<<<<<<< HEAD
 	//this is the line to genjam, added @GH, added @VM
 	//TR::bselect
 	int a, b, x;
+||||||| merged common ancestors
+	//this is the line to genjam, added @GH
+	//TR::bternaries etc.
+	int a, b;
+=======
+	//this is the line to genjam, added @GH, added @VM
+	//TR::bternary
+	//TR::bternaries etc.
+	int a, b;
+>>>>>>> This commit is introduced for the genjam msg from VM
 	return 0;
 }
